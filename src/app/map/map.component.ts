45c9f1bd9d706eac1f6bf925b555a8faa1022c5f
alegzandach <alex.deutch@gmail.com>
2020-01-17 12:34:34 -0500

    add location searching and navigation

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -11,31 +11,46 @@ import * as mapboxgl from 'mapbox-gl';
 })
 
 export class MapComponent implements OnInit{
+
+    constructor(public mapService: MapService) {}
+
     map: mapboxgl.Map;
-    searchText: ''
+    searchText: '';
+    results: {};
+    possibleLocs: [];
     style = 'mapbox://styles/mapbox/streets-v11';
-    lat = 42.36;
-    lng = -71.06;
+    initLat = 42.36;
+    initLng = -71.06;
 
     ngOnInit() {
         mapboxgl.accessToken = environment.mapbox.accessToken;
-          this.map = new mapboxgl.Map({
+        this.map = new mapboxgl.Map({
             container: 'map',
             style: this.style,
             zoom: 13,
-            center: [this.lng, this.lat]
+            center: [this.initLng, this.initLat]
         });
     }
-
-    constructor(public mapService: MapService) {}
 
     public sendSearch() {
         const baseUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/';
         const encodedLoc = encodeURIComponent(this.searchText);
         const token = '.json?access_token=' + environment.mapbox.accessToken;
         var url = baseUrl + encodedLoc + token;
-        this.mapService.getCoords(url).subscribe(data => {
-            console.log(data);
+        if(encodedLoc){
+            this.mapService.getCoords(url).subscribe(data => {
+                //console.log(data)
+                this.results = data;
+            });
+        }else{
+            this.results.features = [];
+        }
+    }
+
+    public newLoc(center) {
+        console.log('hit')
+        this.map.jumpTo({
+            center: center
         });
     }
 }
